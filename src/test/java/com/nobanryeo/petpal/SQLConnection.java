01af6abba0d61687003bae451eb9cc5a92bc9471
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-06-24 15:29:10 +0900

    210624 - master commit(2)

--- src/test/java/com/nobanryeo/petpal/SQLConnection.java
+++ src/test/java/com/nobanryeo/petpal/SQLConnection.java
@@ -1,5 +1,27 @@
-package com.nobanryeo.petpal;
+import java.sql.Connection;
 
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/resources/spring/**/root-context.xml" })
 public class SQLConnection {
 
+      @Inject
+       private DataSource ds;
+    
+       @Test
+       public void testConnection() throws Exception {
+    
+           try (Connection con = ds.getConnection()) {
+    
+               System.out.println("\n >>>>>>>>>> Connection 출력 : " + con + "\n");
+    
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+       }
 }
