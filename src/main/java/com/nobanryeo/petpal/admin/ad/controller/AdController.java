999205bf2f4d357663cf387409aebedd8de42ae1
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-06-24 17:48:28 +0900

    haein commit - 1

--- src/main/java/com/nobanryeo/petpal/admin/ad/controller/AdController.java
+++ src/main/java/com/nobanryeo/petpal/admin/ad/controller/AdController.java
@@ -1,5 +1,15 @@
 package com.nobanryeo.petpal.admin.ad.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/*")
 public class AdController {
 
+	@GetMapping("/main/adApproveList")
+	public void adApproveList() {}
+	
+	
 }
