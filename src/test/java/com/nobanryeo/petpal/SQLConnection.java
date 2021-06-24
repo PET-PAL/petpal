23ee7ab4d3287bb72ec907c0e08469f5e632b2ab
haein-side <kimhi1640@gmail.com>
2021-06-24 15:41:09 +0900

    test code

--- src/test/java/com/nobanryeo/petpal/SQLConnection.java
+++ src/test/java/com/nobanryeo/petpal/SQLConnection.java
@@ -1,3 +1,5 @@
+package com.nobanryeo.petpal;
+
 import java.sql.Connection;
 
 import javax.inject.Inject;
@@ -5,6 +7,8 @@ import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/resources/spring/**/root-context.xml" })
